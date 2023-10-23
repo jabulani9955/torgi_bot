ac8b5af1473d14fa9b8e20341cb2a3c53245a68f
jabulani9955 <denisnovikov1997@gmail.com>
2023-10-23 20:37:29 +0300

    notebook changed

--- __main__.py
+++ __main__.py
@@ -6,6 +6,7 @@ from fake_useragent import UserAgent
 
 
 def collect_data():
+    """ Collect railway stations. """
     current_time = datetime.datetime.now().strftime('%d.%m.%Y, %H:%M')
     print(current_time)
     ua = UserAgent()
@@ -14,17 +15,22 @@ def collect_data():
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
         "User-Agent": ua.random,
     }
-    cookies = {
-        'SESSION': 'OTRhNWM2YzQtZWZkMi00N2Y0LTlkODgtNjNjN2FlNzgxODc2',
-        '_ym_uid': '1691681502844862233',
-        '_ym_d': '1691681502',
-        '_ym_visorc': 'w',
-        '_ym_isad': '1'
-    }
 
-    # response = requests.get('https://torgi.gov.ru/new/public/lots/reg', headers=headers, cookies=cookies)
-    # with open('index.html', 'w', encoding="utf-8") as f:
-    #     f.write(response.text)
+    url = r"http://osm.sbin.ru/esr/region:mosobl:l"
+    response = requests.get(url, headers=headers)
+
+    with open('index.html', 'w', encoding="utf-8") as f:
+        f.write(response.text)
+
+
+def parse_data(parsing_file: str):
+    with open(parsing_file, encoding="utf-8") as f:
+        data = f.read()
+
+    soup = BeautifulSoup()
+    
+    
+
 
 def main():
     collect_data()
